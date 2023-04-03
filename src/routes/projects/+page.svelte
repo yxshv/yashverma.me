ca5dcd856b2b18962e2dc0846b3423b4bec0351e
yxshv <yashverma30115@gmail.com>
2023-04-04 03:15:21 +0530

    project page

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import BackToHome from "../../components/BackToHome.svelte";
+
+</script>
+<div class="lg:p-20 p-10">
+    <BackToHome />
+
+    <h1 class="mt-10 text-5xl font-semibold"><span class="scale-75 opacity-50">#</span> Projects</h1>
+</div>
